153c7184f88ed48ebcbd68d16db852fe2cdb5949
Laurence Trippen <laurence.trippen@gmail.com>
2019-02-07 12:23:48 +0100

    inital commit

---
+++ Assets/Unity-PulseMarker/Examples/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 40f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.down, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Unity-PulseMarker/Scripts/PulseMarker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseMarker : MonoBehaviour
+{
+    public float rotationXOffest;
+
+    private void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        transform.Rotate(transform.rotation.x + rotationXOffest, 0, 0);
+    }
+}
